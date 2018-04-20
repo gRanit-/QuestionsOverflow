df965f99126a1a4156bb66688ea0cd25a6442323
Wojciech Granicki <wojciechgranicki@MacBook-Pro-Wojciech.local>
2018-04-20 16:13:17 +0200

    Initial Commit

---
+++ src/main/java/com/granicki/questionsoverflow/QuestionsoverflowApplication.java
@@ -0,0 +1,12 @@
+package com.granicki.questionsoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsoverflowApplication.class, args);
+	}
+}
